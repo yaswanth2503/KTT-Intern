8ea0c714a335e0b81be76ace430a61a7b4069f13
yaswanth2503 <syaswanth2503@gmail.com>
2025-04-07 11:38:00 +0530

    Iterables and Array.from method

---
+++ Learnings/JAVASCRIPT/Iterables.js
@@ -0,0 +1,35 @@
+
+// Iterables
+
+const arr=[1,2,3,4,5];
+const arrIter=arr[Symbol.iterator]();
+
+console.log(arrIter.next().value);
+console.log(arrIter.next().value);
+console.log(arrIter.next().value);
+console.log(arrIter.next().value);
+console.log(arrIter.next().value);
+console.log(arrIter.next());
+
+const iterator=arr[Symbol.iterator]();  
+let result=iterator.next();
+
+while(!result.done){
+    const element=result.value;
+    console.log(element);
+    result=iterator.next();
+}
+
+
+
+// Array.from() method
+let str = '𝒳😂';
+
+// splits str into array of characters
+let chars = Array.from(str);
+
+console.log(chars[0]); // 𝒳
+console.log(chars[1]); // 😂
+console.log(chars.length); // 2
+
+
