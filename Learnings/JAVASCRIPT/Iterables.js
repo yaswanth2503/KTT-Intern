ebee2708236a9ed9c6fada65b32bc7b9bda815b1
yaswanth2503 <syaswanth2503@gmail.com>
2025-04-07 16:13:23 +0530

    Smart_Function_Parameters

--- Learnings/JAVASCRIPT/Iterables.js
+++ Learnings/JAVASCRIPT/Iterables.js
@@ -305,5 +305,26 @@ let options = {
   console.log(age); // 30
   console.log(city); // New York
   console.log(country); // USA
+
+  // smart function parameters
+  console.log("-------------------smart function parameters-------------------");
+  
+  let options3 = {
+    title: "My menu",
+    items: ["Item1", "Item2"]
+  };
+  
+  function showMenu({
+    title = "Untitled",
+    width: w = 100,  // width goes to w
+    height: h = 200, // height goes to h
+    items: [item1, item2] // items first element goes to item1, second to item2
+  }) {
+    console.log( `${title} ${w} ${h}` ); // My Menu 100 200
+    console.log( item1 ); // Item1
+    console.log( item2 ); // Item2
+  }
+  
+  showMenu(options3);
  
  
